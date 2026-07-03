56a1e59860687442d2f206c9562241e5851a9b78
Jonathan Maron <jonathan.maron@theimagingsource.com>
2026-07-03 09:02:20 +0200

    ♻️ refactor: Added #[\Override] to process() for PHP 8.4/8.5

--- src/PhpConfigMiddleware.php
+++ src/PhpConfigMiddleware.php
@@ -10,6 +10,7 @@ use Psr\Http\Server\RequestHandlerInterface;
 
 class PhpConfigMiddleware extends AbstractPhpConfigMiddleware
 {
+    #[\Override]
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         $config = $this->getConfig();
